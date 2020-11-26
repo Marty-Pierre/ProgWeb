abea70355fabf8c5f32542ebc203a68018e547e9
marty <pierremarty.54@orange.fr>
2020-11-26 12:12:42 +0100

    Ajout de la page principale (non terminée

---
+++ FrontPage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+      <title>Boissons</title>
+	  <meta charset="utf-8" />
+	<?php
+		require_once("Donnees.inc.php");
+		function afficherListeBoissons($tabl){
+			echo "<ol>";
+				foreach($tabl as $elem){
+					echo "<li>".$elem[titre]."<ul>";
+					//echo "<img src='Photos/".$elem[titre].".jpg'>";
+					echo "<li>".$elem[ingredients]."</li>";
+					echo "<li>".$elem[preparation]."</li><br>";
+					echo "</ul></li>";
+				}
+			echo"</ol>";
+		}
+	?>
+		
+</head>
+<body>
+<h1>TEST</h1>
+<?php afficherListeBoissons($Recettes); ?>
+</body>
+</html>
