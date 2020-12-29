4e77510607a4f147269bcb841f7651bba92fa43d
marty <pierremarty.54@orange.fr>
2020-12-29 23:09:15 +0100

    Debut de la premiere fonctionnalité

--- FrontPage.php
+++ FrontPage.php
@@ -6,7 +6,7 @@
 	  <meta charset="utf-8" />
 	<?php
 		require_once("Donnees.inc.php");
-		function afficherListeBoissons($tabl){
+		function afficherListeBoissonsOld($tabl){
 			echo "<ol>";
 				foreach($tabl as $elem){
 					echo "<li>".$elem[titre]."<ul>";
@@ -14,15 +14,39 @@
 					echo "<li>".$elem[ingredients]."</li>";
 					echo "<li>".$elem[preparation]."</li><br>";
 					echo "</ul></li>";
-					echo "lol";
 				}
 			echo"</ol>";
 		}
+
+		function afficherListeBoissons($tabl){
+        			echo "<ol>";
+        				foreach($tabl as $elem){
+        					echo "<li>".$elem[titre]."<ul>";
+        					//echo "<img src='Photos/".$elem[titre].".jpg'>";
+        					echo "<li>".$elem[ingredients]."</li>";
+        					echo "<li>".$elem[preparation]."</li><br>";
+        					echo "</ul></li>";
+        				}
+        			echo"</ol>";
+        		}
 	?>
 		
 </head>
+
 <body>
 <h1>TEST</h1>
+
+<select id="select" onchange="...">
+    <option value="">
+    <?php
+        require_once("Donnees.inc.php");
+        foreach($Hierarchie as $cle => $elem){
+            echo"<option value=".$cle.">".$cle;
+        }
+        ?>
+</select>
+
 <?php afficherListeBoissons($Recettes); ?>
+
 </body>
 </html>
