0b89bc689a0a1f1e495a58a124be58b7732e4c0c
marty <pierremarty.54@orange.fr>
2021-01-03 21:38:36 +0100

    fin normalement

--- JSFiles/FrontPage.js
+++ JSFiles/FrontPage.js
@@ -1,3 +1,4 @@
+//Fonction qui creer la liste deroulante au chargement de la page
 function initListAliments(){
 
     var div = document.getElementById("Selection_aliments");
@@ -5,10 +6,12 @@ function initListAliments(){
     var sel = document.createElement("select");
     sel.id = "selectAliment_1";
     sel.setAttribute("onchange","choisirAliments(value,this)");
+    //Creation d'une option vide pour retourner a l'ensemble de toutes les recettes
     optEmpty = document.createElement("option");
-    optEmpty.value = "";
-    optEmpty.text = "";
+    optEmpty.value = "Aliment";
+    optEmpty.text = "Tout";
     sel.append(optEmpty);
+    //Creation des options qui sont les sous categorie direct de Aliment
     for(alim of hierarchie["Aliment"]["sous-categorie"]){
         var opt = document.createElement("option");
         opt.value = alim;
@@ -19,34 +22,38 @@ function initListAliments(){
     div.append(sel);
 }
 
+//Fonction qui gere le fonctionnement de la liste deroulante (en creer de nouvelles, en supprimer ...)
 function choisirAliments(aliment,liste){
 
             var div = document.getElementById("Selection_aliments");
             var tablSelect = div.getElementsByTagName("select");
-            var numListe = parseInt(liste.id.split("_")[1]);
+            var numListe = parseInt(liste.id.split("_")[1]); //On recupere l'id du select qui appelle la fonction
 
+            //On enleve les listes deroulantes "filles" du select qui appelle la fonction
             while(tablSelect.length > numListe){
                 tablSelect[numListe].remove();
                 }
 
-
-            if(aliment != ""){
+            //Bloc de code qui creer la nouvelle liste deroulante
+            if(aliment != "Aliment"){
             for(var [key, categorie] of Object.entries(hierarchie)){
 
-                    if(key ==aliment){
+                    if(key == aliment){
 
                         for(var [categ, tablElem] of Object.entries(categorie)){
 
                          if(categ === "sous-categorie"){
-
+                            //On creer la liste deroulante avec comme options les sous-categories de l'aliment selectionnee
                             var sel = document.createElement("select");
                             sel.id = "selectAliment_" + (numListe + 1);
                             sel.setAttribute("onchange","choisirAliments(value,this)");
 
+                            //Creation d'une option vide pour des raisons esthetiques
                             var optEmpty = document.createElement("option");
                             optEmpty.value = "";
                             optEmpty.text = "";
                             sel.append(optEmpty);
+                            //On ajoute dans la nouvelle liste deroulante toutes les sous-categorie de l'aliment
                             for(var elem of Object.values(tablElem)){
 
                                 var opt = document.createElement("option");
@@ -63,53 +70,90 @@ function choisirAliments(aliment,liste){
                     }
                 }
             }
-
+            updateTable(aliment); // On met a jour le tableau de boissons
 }
 
-
+//Fonction qui met a jour le tableau de boissons
 function updateTable(aliment){
-        var tabl = document.getElementById("Tableau_recettes");
-       tabl.innerHTML = "<tr> <th> Boisson </th> <th> Ingredients </th> <th> Preparation </th> </tr>";
+       var tabl = document.getElementById("Tableau_recettes");
+       tabl.innerHTML = "<tr> <th> Boisson </th> <th> Ingredients </th> <th> Preparation </th> <th> Photo </th></tr>";
+       //Parcours de toutes les boissons
         for(var boissonInfos of Object.values(recettes)){
-
+            var appartient = false;
+            //On regarde si un ingredient de la boisson est de la famille de l'aliment qui est selectionne
             for(var boissonAliment of Object.values(boissonInfos["index"])){
                 if(estDeLaFamilleDe(boissonAliment,aliment)){
-                    var tr = document.createElement("tr");
-                    var boi = document.createElement("td");
-                    var ing = document.createElement("td");
-                    var prep = document.createElement("td");
-                    boi.innerHTML = "oui";
-                    ing.innerHTML = "oui";
-                    prep.innerHTML = "oui";
-                    tr.append(boi);
-                    tr.append(ing);
-                    tr.append(prep);
-                    tabl.append(tr);
+                    var appartient = true;
                  }
-
             }
+            //Bloc de code qui permet d'afficher la boisson dans le tableau, si elle doit etre affichee
+            if(appartient){
+                 var tr = document.createElement("tr");
+                 var boi = document.createElement("td");
+                 var ing = document.createElement("td");
+                 var prep = document.createElement("td");
+                 var photo = document.createElement("td");
+
+                var img = document.createElement("img");
+                img.setAttribute('onerror','this.style.display="none"');
+                imgPath = boissonInfos["titre"];
+                imgPath = imgPath.charAt(0) + imgPath.slice(1).toLowerCase(); //Modification de la casse pour correspondre au format de nom des images
+                var imgPath = "/Photos/" + imgPath + ".jpg";
+                imgPath = replaceSpecialChar(imgPath); //Necessaire pour les boissons comme la Pina Collada avec la egne
+                img.setAttribute("src", imgPath);
+
+
+                boi.innerHTML = boissonInfos["titre"];
+                ing.innerHTML = boissonInfos["ingredients"].replace(/\|/g,"<br>");
+                prep.innerHTML = boissonInfos["preparation"];
+                photo.append(img);
+                tr.append(boi);
+                tr.append(ing);
+                tr.append(prep);
+                tr.append(photo);
+                tabl.append(tr);
+             }
         }
 }
-
+//Fonction qui verifie si un aliment est une sous_categorie, direct ou indirect, d'un autre aliment
 function estDeLaFamilleDe(ss_cat,famille){
     if (ss_cat == famille){
         return true;
     }
-    //Sous categorie different de la famille
+
+    //Sous categorie est donc different de la famille
 
     if(famille == "Aliment"){
         return true;
     }
-    //famille n'est pas l'ensemble des aliments
 
-    if(ss_cat.length == 1){
-        return false;
+    //famille n'est donc pas l'ensemble des aliments
+
+    if(ss_cat == "Aliment"){
+        return false
     }
-    //La sous_categorie n'est pas une feuille ou l'ensemble des aliments
 
-    var tab = hierarchie[ss_cat]["super-categorie"];
-    document.write(tab[0]);
+    //Fin de la recherche, "ss_cat" n'est donc pas de la famille de "famille"
+
+    //On rapelle la fonction sur les super categorie de l'aliment
+   var tab = hierarchie[ss_cat]["super-categorie"];
+   var res = false
    for(var [ind , sup] of Object.entries(tab)) {
-        return estDeLaFamilleDe(sup,famille);
+        res = res || estDeLaFamilleDe(sup,famille);
     }
+    return res;
+}
+
+//Fonction qui transform un string pour qu'il ne possede pas de caracteres speciaux
+function replaceSpecialChar(text){
+    text = text.replace(/ñ/g,"n");
+    text = text.replace(/é/g,"e");
+    text = text.replace(/è/g,"e");
+    text = text.replace(/ê/g,"e");
+    text = text.replace(/à/g,"a");
+    text = text.replace(/î/g,"i");
+    text = text.replace(/ï/g,"i");
+    text = text.replace(/ /g,"_");
+    text = text.replace(/'/g,"");
+    return text;
 }
--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 	  <meta charset="utf-8" />
         <link rel="stylesheet" href="feuillesDeStyle/FrontPageStyle.css">
       <script type="text/javascript" src="JSFiles/FrontPage.js"></script>
-
 	  <script type="text/javascript">
 
+        <!-- Utilisation de json_encode pour pouvoir utiliser les variables php en javascript -->
 	   var hierarchie = <?php require_once("Donnees.inc.php");
        echo json_encode($Hierarchie);
 	   ?>;
@@ -18,22 +18,6 @@
         ?>;
 	   </script>
 
-	<?php
-		require_once("Donnees.inc.php");
-
-		function afficherListeBoissons($tabl){
-
-        			/*	foreach($tabl as $elem){
-        					echo "<li>".$elem["titre"]."<ul>";
-        					//echo "<img src='Photos/".$elem[titre].".jpg'>";
-        					echo "<li>".$elem["ingredients"]."</li>";
-        					echo "<li>".$elem["preparation"]."</li><br>";
-        					echo "</ul></li>";
-        				}
-        			echo"</ol>";*/
-        		}
-
-	?>
 </head>
 
 <body>
